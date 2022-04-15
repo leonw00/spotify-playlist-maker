ba8c9b7a1680cd767cb9cc9ec79d38ad2fb8ce65
leo <leonardow1908@gmail.com>
2022-04-15 01:01:20 -0500

    implement word count

--- src/views/Main/MainPage.js
+++ src/views/Main/MainPage.js
@@ -9,19 +9,28 @@ import "./MainPage.css";
 function MainPage() {
   const [target, setTarget] = useState("");
   const [tracks, setTracks] = useState([]);
+  const [wordCount, setWordCount] = useState(0);
 
-  const generateRandomText = () =>{
+  const generateRandomText = () => {
     let randomText = getRandomText();
+    let wordList = randomText.split(/[ ,.]+/).filter((item) => item);
     setTarget(randomText);
-  }
+    setWordCount(wordList.length);
+  };
 
-  const showTextButtons = () =>{
-    return(
+  const showTextButtons = () => {
+    return (
       <div className="text-area-button-block">
         <TextAreaButton onClick={generateRandomText} name="Sample Text" />
       </div>
     );
-  }
+  };
+
+  const setFormattedContent = (value) => {
+    setTarget(value);
+    let wordList = value.split(/[ ,.]+/).filter((item) => item);
+    setWordCount(wordList.length);
+  };
 
   return (
     <div className="main-page">
@@ -29,21 +38,20 @@ function MainPage() {
 
       <div className="main-body">
         <div className="input-block">
-
-          {!target ? showTextButtons() : ''}
+          {!target ? showTextButtons() : ""}
 
           <textarea
             value={target}
-            onChange={(e) => setTarget(e.target.value)}
+            onChange={(e) => setFormattedContent(e.target.value)}
             className="text-area"
             placeholder="Anything you type will be converted into a spotify playlist."
           ></textarea>
           <div className="button-block">
+            <div className="word-count">{wordCount} / 250</div>
             <RunButton
               name="Generate Playlist"
               onClick={async () => {
                 var trackList = await generateTrackList(target);
-                
                 setTracks(trackList);
               }}
             />
